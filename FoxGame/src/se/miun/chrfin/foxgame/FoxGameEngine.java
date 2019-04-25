21953b66ff42bca6b6ea4ccb2cf1ef07eae300f0
Bernard Che Longho <lobe1602@student.miun.se>
2019-04-25 21:05:35 +0200

    Initial commit with all files and libraries

---
+++ FoxGame/src/se/miun/chrfin/foxgame/FoxGameEngine.java
@@ -0,0 +1,49 @@
+package se.miun.chrfin.foxgame;
+
+import ch.rfin.foxgame.Foxgame;
+import ch.rfin.foxgame.rules.State;
+import se.miun.chrfin.foxgame.com.GameStatus;
+import se.miun.chrfin.foxgame.setup.PlayerSetup;
+
+/**
+ * @author Christoffer Fink
+ */
+public class FoxGameEngine implements AiGameEngine {
+	private final PlayerSetup setup;
+	private State currentState;
+	private Foxgame foxgame;
+
+	public FoxGameEngine(PlayerSetup setup) {
+		this.setup = setup;
+		foxgame = new Foxgame();
+		currentState = foxgame.getRoot();
+
+	}
+
+	/**
+	 * Return a move of the form "x1,y1 x2,y2".
+	 */
+	@Override
+	public String getMove(GameStatus status) {
+		/**
+		 * There must be some logic here that one has to do in order for this
+		 * thing to work well
+		 */
+		System.out.println(status.move);
+		return status.move;
+	}
+
+	@Override
+	public void updateState(String move) {
+		if (foxgame.terminal(currentState))
+			return;
+
+		currentState = foxgame.transition(currentState, move);
+	}
+
+	@Override
+	public String getPlayerName() {
+		return setup.playerName;
+	}
+
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# AI project
+Implementing the FoxGame
+
+## Rules 
+See [foxgame rules](instructions/FoxGame-rules.pdf) (pdf)
+
+## Guides and associated libraries
+See [foxgame info](instructions/FoxGame-info.pdf) (pdf)
